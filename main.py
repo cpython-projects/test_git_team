d90d2ff430e8d577cdfbc81b51d8ec1d6a8a8a35
OST <oleh.s.tymchuk@gmail.com>
2024-07-13 19:01:24 +0200

    add base structure

---
+++ main.py
@@ -0,0 +1,14 @@
+def full_name(first, last):
+    ...
+
+
+def current_month():
+    ...
+
+
+def main():
+    print(f"Hello, {full_name('John', 'Doe')}!")
+    print(f"Today is {current_month()}.")
+
+
+if __name__ == "__main__":
